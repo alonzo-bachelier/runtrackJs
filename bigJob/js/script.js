c5b231d66b6a79ab44a559e5c62598ca28adcc0b
alonzo-bachelier <alonzo.bachelier@laplateforme.io>
2024-04-05 12:12:36 +0200

    creation du header et mise en forme

---
+++ bigJob/js/script.js
@@ -0,0 +1,6 @@
+const prenom = $("#prenom");
+const nom = $("#nom");
+const email = $("#mail");
+const mdp = $("#mot-de-passe");
+const mdrVerif = $("#mdp-verif");
+const btnVerif = $("#bouton-confirmer");
